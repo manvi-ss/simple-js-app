767406e0db286fb47817e9de606d071e732ae4e5
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-05-15 15:49:04 +0200

    updated javascript with content

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 alert('Hello World');
+
+//variable
+let myName = 'Manvi ';
+document.write(myName);
+let myLike= 'likes ';
+document.write(myLike);
+let favoriteFood = 'rice';
+document.write(favoriteFood);
