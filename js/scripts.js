4ddbebf3ddbb0635a090059cc41a48b78f57a612
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-05-15 15:29:34 +0200

    alert function added

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,1 @@
-// let myName = 'Manvi';
-// document.write(myName);
-// myName = 'Manvi Singh';
-// document.write(myName);
-
-// let simpleAddition = 2+2;
-// document.write(simpleAddition);
-
-// let size = 100;
-// let doubleSize = size *2;
-// document.write(doubleSize);
-
-let size= 100;
-let doubleSize = size*2;
-let minSize = (doubleSize*2)-(size/2);
-document.write(minSize);
+alert('Hello World');
