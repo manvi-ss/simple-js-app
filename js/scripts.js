985d6317e95f75f45d8fea417fba7d52526ba378
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-05-28 15:51:14 +0200

    Repository with IIFE is created
    Repository with IIFE is created and for each() function is used instead of for loop. add and get all functions are returned.

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,67 @@
-//pokemon Application
-let pokemonList = [
-    {
-        number:1, 
-        name: 'Bulbasaur', 
-        height: .7, 
-        type:['grass', 'poison']
-    },
-    {
-        number:2, 
-        name: 'Charizard', 
-        height: 1.7, 
-        type:['flying','fire']
-    },
-    {
-        number:3, 
-        name: 'Butterfree', 
-        height: 1.1, 
-        type:['bug','flying']
-    },
-    {
-        number:4, 
-        name: 'Pikachu', 
-        height: .4, 
-        type:['electric']
+//pokemon Application//
+
+let pokemonRepository = (function() {// wrapping the pokeonList inside of an IIFE (Immediately Invoked Function Expression)
+    let pokemonList = [ // database of pokemon for the pokedex
+        {
+            number:1, 
+            name: 'Bulbasaur', 
+            height: .7, 
+            type:['grass', 'poison']
+        },
+        {
+            number:2, 
+            name: 'Charizard', 
+            height: 1.7, 
+            type:['flying','fire']
+        },
+        {
+            number:3, 
+            name: 'Butterfree', 
+            height: 1.1, 
+            type:['bug','flying']
+        },
+        {
+            number:4, 
+            name: 'Pikachu', 
+            height: .4, 
+            type:['electric']
+        }
+    ];
+    function add(pokemon){
+        pokemonList.push(pokemon);
     }
-];
+    function getAll(){
+        return pokemonList;
+    }
+    return { //returning add & getall functions so that they maybe used outside the IIFE to acess pokemon lost
+        add: add,
+        getAll: getAll
+        }
+})();
+// console.log(pokemonRepository.getAll());
+pokemonRepository.add({number:5, name:'Squirtle', height: .5, type:['water'] });//adding new item to pokemon array using add function. As add function is returning it is accessible to add new item.
+// console.log(pokemonRepository.getAll());
 
-
-for (let i = 0; i < pokemonList.length; i++){
-    //to write on website with concatenation.
-
+function listPokemon(pokemon){// this function will list the details of pokemon
     document.write( 
-       `# ${pokemonList[i].number} ${pokemonList[i].name}  (Height: ${pokemonList[i].height}) <br>` // br here will begin next step in new line. only number name an dheight is printed in single line.
-        
-    )
+        `# ${pokemon.number} ${pokemon.name}  (Height: ${pokemon.height}) <br>`);
 
-    if (pokemonList[i].type.length <= 1) { // conditional to get type of pokemon
-        document.write(
-            ` Type: ${pokemonList[i].type[0]} -- wow! This Pokemon has One type `// this will print pokemon with one type
-        )
-    } else {
-        document.write(
-            `Type: ${pokemonList[i].type[0]} , ${pokemonList[i].type[1] }`// this will print pokemon with two types
-        )
-    }
-
-    if (pokemonList[i].height > 1.5) { // height of pokemon
-        document.write(
-            ` <br> Height: ${pokemonList[i].height} -- wow ! This pokemon is big ` // <br>gives new line to height and the comment related to height
-        )}
-         
- document.write("<br><br>") //leave space between each iterations
+        if (pokemon.type.length <= 1) { // conditional to get type of pokemon
+            document.write(
+                ` Type: ${pokemon.type[0]} -- wow! This Pokemon has One type `// this will print pokemon with one type
+            )
+        } else {
+            document.write(
+                `Type: ${pokemon.type[0]} , ${pokemon.type[1] }`// this will print pokemon with two types
+            )
+        }
+        if (pokemon.height > 1.5) { // height of pokemon
+            document.write(
+                ` <br> Height: ${pokemon.height} -- wow ! This pokemon is big ` )// <br>gives new line to height and the comment related to height
+        }
+    document.write("<br><br>") //leave space between each iterations
     
+    }; // br here will begin next step in new line. only number name an dheight is printed in single line.
+   pokemonRepository.getAll().forEach(listPokemon);// running the listPokemon function for each pokemon returned by pokemonRespository.getAll()
 
-}
+
