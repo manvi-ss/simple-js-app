ba87580779c7467873821ddf0cc2f3b3054c11ed
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-05-31 16:16:08 +0200

    even listner and DOM manipulation is done by adding buttons
    dom manipulation is done

--- js/scripts.js
+++ js/scripts.js
@@ -31,42 +31,40 @@ let pokemonRepository = (function() {// wrapping the pokeonList inside of an IIF
         if (typeof pokemon === 'object'){
             pokemonList.push(pokemon);
         } else{
-            console.log('pokemon input is not correct')
+            console.log('pokemon input is not correct');
         }
         
     }
     function getAll(){
         return pokemonList;
     }
+    
+    function addListItem(pokemon){
+        let ul = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText =  pokemon.name;
+        button.classList.add('pokemonButton');
+        listItem.appendChild(button);
+        ul.appendChild(listItem);
+        
+        button.addEventListener('click',function(event){
+            showDetails(pokemon)
+        });
+    }
+    function showDetails(pokemon){
+        console.log(pokemon);
+    }
     return { //returning add & getall functions so that they maybe used outside the IIFE to acess pokemon lost
         add: add,
-        getAll: getAll
-        }
+        getAll: getAll,
+        addListItem: addListItem
+        };
 })();
 // console.log(pokemonRepository.getAll());
 pokemonRepository.add({number:5, name:'Squirtle', height: .5, type:['water'] });//adding new item to pokemon array using add function. As add function is returning it is accessible to add new item.
-// console.log(pokemonRepository.getAll());
+console.log(pokemonRepository.getAll());
 
-function listPokemon(pokemon){// this function will list the details of pokemon
-    document.write( 
-        `# ${pokemon.number} ${pokemon.name}  (Height: ${pokemon.height}) <br>`);
-
-        if (pokemon.type.length <= 1) { // conditional to get type of pokemon
-            document.write(
-                ` Type: ${pokemon.type[0]} -- wow! This Pokemon has One type `// this will print pokemon with one type
-            )
-        } else {
-            document.write(
-                `Type: ${pokemon.type[0]} , ${pokemon.type[1] }`// this will print pokemon with two types
-            )
-        }
-        if (pokemon.height > 1.5) { // height of pokemon
-            document.write(
-                ` <br> Height: ${pokemon.height} -- wow ! This pokemon is big ` )// <br>gives new line to height and the comment related to height
-        }
-    document.write("<br><br>") //leave space between each iterations
-    
-    }; // br here will begin next step in new line. only number name an dheight is printed in single line.
-   pokemonRepository.getAll().forEach(listPokemon);// running the listPokemon function for each pokemon returned by pokemonRespository.getAll()
-
-
+pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+});
